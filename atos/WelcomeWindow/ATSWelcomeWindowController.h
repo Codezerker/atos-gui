9f71c0b62b28f693467482f45a291077c9145c74
Yan Li <eyeplum@gmail.com>
2014-04-20 14:44:13 +0800

    Add WelcomWindowController.

---
+++ atos/WelcomeWindow/ATSWelcomeWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ATSWelcomeWindowController.h
+//  atos
+//
+//  Created by eyeplum on 4/20/14.
+//  Copyright (c) 2014 eyeplum. All rights reserved.
+//
+
+
+
+@interface ATSWelcomeWindowController : NSWindowController
+
+@end
---
+++ atos/WelcomeWindow/ATSWelcomeWindowController.m
@@ -0,0 +1,33 @@
+//
+//  ATSWelcomeWindowController.m
+//  atos
+//
+//  Created by eyeplum on 4/20/14.
+//  Copyright (c) 2014 eyeplum. All rights reserved.
+//
+
+#import "ATSWelcomeWindowController.h"
+
+@interface ATSWelcomeWindowController ()
+
+@end
+
+@implementation ATSWelcomeWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
